97255af03058ca4109278d2ab8ab4b50344422f3
White Dog <dlrlgus1204@naver.com>
2021-11-26 16:50:00 +0900

    0.3 Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,42 +1,63 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
-        const {target : {name, value}} = event;
-        if( name === "email") {
+        const { target: { name, value } } = event;
+        if (name === "email") {
             setEmail(value);
-        } else if ( name === "password"){
+        } else if (name === "password") {
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                // create account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // login
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            setError(error.message);
+        }
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+    
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input
-                name="email"
-                type="text" 
-                placeholder="Email" 
-                required
-                value={email}
-                onChange={onChange}
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    required
+                    value={email}
+                    onChange={onChange}
                 />
                 <input
-                name="password"
-                type="password" 
-                placeholder="Password" 
-                required
-                value={password}
-                onChange={onChange}
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={password}
+                    onChange={onChange}
                 />
-                <input type="submit" placeholder="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Login"} />
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Signin" : "Create Account"} </span>
             <div>
                 <button>Continue with Google</button>
+                {error}
             </div>
         </div>
     )
