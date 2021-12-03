0da9a53d3bb4bf291313a687cf341695e4f6057b
White Dog <dlrlgus1204@naver.com>
2021-12-03 17:34:36 +0900

    1.1 Style Base Update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import {
+    faHome,
+    faUserAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+import '../css/navigation.scss';
+
 const Navigation = ({ userObj }) => (
     <nav>
         <ul>
             <li>
-                <Link to="/">Home</Link>
+                <Link to="/">
+                    <FontAwesomeIcon icon={faTwitter} className="twitter" />
+                </Link>
             </li>
             <li>
-                <Link to="/profile">{userObj?.displayName ? `${userObj.displayName}의 프로필` : `유저의 프로필`}</Link>
+                <Link to="/">
+                    <FontAwesomeIcon icon={faHome} /> <p>Home</p>
+                </Link>
+            </li>
+            <li>
+                <Link to="/profile"><FontAwesomeIcon icon={faUserAlt} /> <p>{userObj?.displayName ? `${userObj.displayName}의 프로필` : `유저의 프로필`}</p></Link>
             </li>
         </ul>
     </nav>
